475721b3142dadc2bb3c411b03c4dcfa01da5b63
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-05 03:51:55 +0100

    Fixed type

--- zomes/zomepository/src/template_dna.rs
+++ zomes/zomepository/src/template_dna.rs
@@ -2,7 +2,7 @@ use hdk3::prelude::*;
 
 #[hdk_entry(id = "template_dna")]
 pub struct TemplateDna {
-    zomes: Vec<EntryHash>, // TODO: change to map?
+    zomes: Vec<(String, EntryHash)>, // TODO: change to map?
 }
 
 #[hdk_entry(id = "instantiated_dna")]
